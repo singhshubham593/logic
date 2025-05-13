2c0979457b7cea987b308c3336cc70a5ca56714c
singhshubham593 <rajputshubhamsingh593@gmail.com>
2025-05-13 21:15:31 +0530

    Take input and print sum

---
+++ logicDayOne/InputSumPrint.js
@@ -0,0 +1,16 @@
+// Problem Statement -2
+// Write a function that takes two numbers as input and prints their sum.
+
+function a(x,y){
+    const sum= x + y;
+    console.log("Sum is :",sum);
+}
+
+a(3,4);   //7
+a(500,600);  //11
+a(6,-2);    //4
+a(-1,-5);   //-6
+a(-1,0);    //-1
+a(1,0);     //1
+a(1,1.5);   //2.5
+a(2.5,1.5);  //4
