8a6103524644f097f47b90e6b8c2736702202c59
singhshubham593 <rajputshubhamsingh593@gmail.com>
2025-05-15 22:08:05 +0530

    Update FindSmallestNo.js

--- logicDayOne/FindSmallestNo.js
+++ logicDayOne/FindSmallestNo.js
@@ -1,5 +1,11 @@
 // Problem Statement -5
 // Write a function that finds and prints the smallest numbers among three given numbres.
-function smallest(){
+function smallest(a,b,c){
+    smal=a;
+    if(smal>b){
+        smal=b;
+    }if (smal>c) {
+        smal=c
+    }  
 
 }
