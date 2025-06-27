5eb047ba07d3f77641d04991e90af552631405b2
singhshubham593 <92996831+singhshubham593@users.noreply.github.com>
2025-06-27 22:48:42 +0530

    multiplication table in range

---
+++ logicDayOne/MuitinTable.js
@@ -0,0 +1,6 @@
+// write a fun that generate and print a multiplication table for a given no to a specific range
+//Input: 2 +ve no
+//  -Table of
+//  - Table till
+//Return:Nothing only print the result
+// Example: 4,8=> 4,8,12,16....32
