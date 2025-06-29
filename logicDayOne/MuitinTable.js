643d15921c3bb170f7ea49e6b2f758257735fb42
singhshubham593 <92996831+singhshubham593@users.noreply.github.com>
2025-06-29 20:29:56 +0530

    multiplication table for a given no to a specific range

--- logicDayOne/MuitinTable.js
+++ logicDayOne/MuitinTable.js
@@ -4,3 +4,14 @@
 //  - Table till
 //Return:Nothing only print the result
 // Example: 4,8=> 4,8,12,16....32
+
+
+function mul(no,re){
+     let n=0;
+    for(let i=1;i<=re;i++){
+        n=no*i;
+        console.log(`${no}*${i}=${n}`); // `` this is String litteral,,${} this is for string represention
+    }
+}
+
+console.log(mul(4,8))
