cbc4ac1ad7ec2e400214e52737e63d285baabf2f
singhshubham593 <92996831+singhshubham593@users.noreply.github.com>
2025-06-29 20:30:49 +0530

    calculates and print the sum of digits of a given no

--- logicDayOne/SumDigit.js
+++ logicDayOne/SumDigit.js
@@ -2,5 +2,28 @@
 // Input: A no,+ve integer->12,34,123
 // return: sum of all digits 12:1+2=3
 
-
+//1st way - with String
+function sums(no){
+    let sum=0;
+    let strconvert=no.toString(); // toString= use to convert no into stingh
+    let sp=strconvert.split('');  // split('')= use to split stringh
+    sp.forEach(num => {           // forEach= use to itterate on each element
+        sum= sum+parseInt(num);  // parseInt= use to convert String into Integer
+    });
+    return sum;
+     
+}
+//console.log(parseInt(sums(23)));
  
+// 2nd way-without String- with %
+function s(no){
+    let n = no;
+    let su=0;
+    while(n > 0){
+        let ne=n%10; // here % use to get last digit of no
+        n=Math.floor(n/10); //Math.floor=use to renove decimal point and make it no like 1.2 will become 1
+        su=su + ne;
+    }
+    return su;
+}
+console.log(s(123));
