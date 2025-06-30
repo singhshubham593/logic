7b94ce53729afd400a2779bd743761db903e3d99
singhshubham593 <92996831+singhshubham593@users.noreply.github.com>
2025-06-30 22:12:42 +0530

    Create Palindrome.js

---
+++ logicDayOne/Palindrome.js
@@ -0,0 +1,5 @@
+//write a fun that checks if a string is palindrome or not
+// input: A string
+// return:boolean,true if Palindrome snd false if not palindrome
+// A palindrome is a word that reads the same backward or forward
+// Example: madam,level,abba
