8158ee3445aa06aeda670cf0acabdf360c2a2d91
singhshubham593 <92996831+singhshubham593@users.noreply.github.com>
2025-06-30 22:12:24 +0530

    Create ExpoCalculator.js

---
+++ logicDayOne/ExpoCalculator.js
@@ -0,0 +1,6 @@
+// write  a function that returns the result of raising a given no to a specified power.
+//input: Two no
+//  Base
+//  Exponent
+// Return:Base to the power of exponent
+// Example: 2 ,3: 2*2*2=8
