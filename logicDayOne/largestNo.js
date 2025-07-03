d30d0f22d55e7b356a1ff6b1c0d9e096e887406e
singhshubham593 <92996831+singhshubham593@users.noreply.github.com>
2025-07-03 22:58:11 +0530

    Largest no in Array

--- logicDayOne/largestNo.js
+++ logicDayOne/largestNo.js
@@ -2,3 +2,24 @@
 // input: Array of numbers
 // Return:Biggest number
 //example: [3,6,9,7,1,8] =>9
+
+//One way
+function long(){
+    const a=[3,5,9,8,7,4,2];
+     let b=a[0];
+     for(let i=1;i<a.length;i++){
+        if(a[i]>b){
+            b=a[i];
+        }
+     }
+     console.log(b);
+}
+long()
+
+//Two way
+function largest(){
+    const c=Math.max(2,6,9,8,7,6,3,1); //max will pick max no from gven data
+    console.log(c);
+}
+ 
+largest()
