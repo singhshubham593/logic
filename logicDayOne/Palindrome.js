9c64129c741734416f3f8cd72441e07ad3c049a4
shubhamurl <92996831+singhshubham593@users.noreply.github.com>
2025-07-04 21:40:28 +0530

    Update Palindrome.js

--- logicDayOne/Palindrome.js
+++ logicDayOne/Palindrome.js
@@ -3,3 +3,19 @@
 // return:boolean,true if Palindrome snd false if not palindrome
 // A palindrome is a word that reads the same backward or forward
 // Example: madam,level,abba
+
+
+function a(b){
+    let c=b.split("");
+    const d=c.length -1; //c.length-1 will give last index
+    for(let i=0;i<d/2;i++){
+        if(c[i]===c[d-i]){  //d-i will reduce 1 from last vale like i=0:d=5 ,i=1:d=5-i(1)=4,i=2:d=5-i(2)=3 
+             return true;
+        }else{
+            return false;
+        }
+         
+    }
+
+}
+console.log(a("amadama"));
