dda60d4e3293661908cd2d58ad2c410add0714f4
shubhamurl <92996831+singhshubham593@users.noreply.github.com>
2025-07-07 22:02:57 +0530

    Create PrimeNoInRange.js

---
+++ logicDayOne/PrimeNoInRange.js
@@ -0,0 +1,5 @@
+// write a fun that give prime no in range of no
+// input: A no, B no
+// return: all print no in this range
+// Prime no: A no that can only be divided by itself and 1 without remainders.
+// example: 3,20 -> 5,7,11,13,17,19
