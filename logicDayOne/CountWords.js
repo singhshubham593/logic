4340f9068e9c66747aacde6edb8bb80551aa3602
shubhamurl <92996831+singhshubham593@users.noreply.github.com>
2025-07-07 22:33:38 +0530

    counts the number of words in a given sentance.

---
+++ logicDayOne/CountWords.js
@@ -0,0 +1,5 @@
+//write a function that counts the number of words in a given sentance.
+//input: A sentance
+// return: number of words in the sentance
+// example: "Hello world ji" -> 3 words
+// hints: 3 words means 2 spaces
--- logicDayOne/PrimeNoInRange.js
+++ logicDayOne/PrimeNoInRange.js
@@ -3,3 +3,16 @@
 // return: all print no in this range
 // Prime no: A no that can only be divided by itself and 1 without remainders.
 // example: 3,20 -> 5,7,11,13,17,19
+
+function prime(n,m){
+    let a=0;
+    for(let i=n;i<=m;i++){
+        if(i/1===0 && i/i===0){
+            console.log(i);
+            a=i;
+
+        }
+    }
+    return a;
+}
+prime(3,20);
